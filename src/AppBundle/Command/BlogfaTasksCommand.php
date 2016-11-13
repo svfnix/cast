2eb37155ed7192a805ad7a568d3069687df0bcce
svfbamilo <saeed.farahani@gmail.com>
2016-11-13 13:46:39 +0330

    add task

--- src/AppBundle/Command/BlogfaTasksCommand.php
+++ src/AppBundle/Command/BlogfaTasksCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DomCrawler\Field\InputFormField;
 
 class BlogfaTasksCommand extends AppCommandBlogfa
 {
