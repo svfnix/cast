03852aeef28536d74903ddf594ea289880d5d247
saeed <psproot@gmail.com>
2016-11-15 00:58:42 +0330

    some emails

--- src/AppBundle/Command/PersianblogSyncCommand.php
+++ src/AppBundle/Command/PersianblogSyncCommand.php
@@ -2,27 +2,25 @@
 
 namespace AppBundle\Command;
 
-use AppBundle\Wrapper\AppCommandBlogfa;
 use Goutte\Client;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Validator\Constraints\DateTime;
 
-class BlogfaSyncCommand extends AppCommandBlogfa
+class PersianblogSyncCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
         $this
-            ->setName('blogfa:sync')
-            ->setDescription('syncing blogfa blog')
+            ->setName('persianblog:sync')
+            ->setDescription('syncing persianblog blog')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
         $em = $this->getContainer()->get('doctrine')->getEntityManager();
 
         $output->writeln("Start syncing");
@@ -68,28 +66,30 @@ class BlogfaSyncCommand extends AppCommandBlogfa
 
         $this->client = new Client();
 
-        $output->writeln(" - signin to blogfa [".$account->getUsername().".blogfa.com]");
+        $output->writeln(" - signin to blogfa [".$account->getUsername().".persianblog.ir]");
         $this->signin($account);
 
         $output->writeln(" - submit new post. wait 10 secs  ...");
-        $crawler = $this->getMenuLink(1);
+        $crawler = $this->client->request('GET', 'http://persianblog.ir/CreatePost.aspx?blogID=' . $account->getBlogId());
 
-        sleep(10);
+        $tags = $article->getTags();
+        $tags = explode(',', $tags);
+
         $form = $crawler->filter('#btnPublish')->first()->form();
         $this->client->submit($form, [
             'txtTitle' => $article->getTitle(),
-            'txtPostBody' => $content,
-            'txtTags' => str_replace(',', '+', $article->getTags())
+            'intrContent' => $content,
+            'keyWord1' => array_shift($tags),
+            'keyWord2' => array_shift($tags),
+            'keyWord3' => array_shift($tags),
+            'keyWord4' => array_shift($tags),
         ]);
 
         $em->remove($article);
         $em->flush();
 
-        $output->writeln(" - post submitted successfully");
-        $output->writeln(" - refresh blog");
-        $this->client->request('GET', 'http://blogfa.com/Desktop/refreshblog.ashx?r='.time());
-
         $output->writeln("Operation completed");
+
     }
 
 }
