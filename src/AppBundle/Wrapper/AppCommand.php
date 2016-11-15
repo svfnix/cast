83d1be61bcaba78769201511b3e86a27a1da5ca1
svfbamilo <saeed.farahani@gmail.com>
2016-11-15 12:47:57 +0330

    add miners

--- src/AppBundle/Wrapper/AppCommand.php
+++ src/AppBundle/Wrapper/AppCommand.php
@@ -19,7 +19,13 @@ class AppCommand extends ContainerAwareCommand
     {
         $this->client = new Client();
 
-        $gclient = new GuzzleClient(array('allow_redirects' => true, 'cookies' => true));
+        $gclient = new GuzzleClient([
+            'allow_redirects' => true,
+            'cookies' => true,
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'
+            ]
+        ]);
         $this->client->getClient()->setCookieJar(
             $this->client->getClient()->getConfig('cookies')->fromArray(
                 [
