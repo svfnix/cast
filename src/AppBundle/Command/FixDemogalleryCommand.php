d4b54c6bbf7896db1ee6dce874c3de93e15d3d87
saeed <psproot@gmail.com>
2016-11-20 17:31:22 +0330

    digiato

--- src/AppBundle/Command/FixDemogalleryCommand.php
+++ src/AppBundle/Command/FixDemogalleryCommand.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Command;
 
 use AppBundle\Wrapper\AppCommand;
+use PhpImap\Exception;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -26,26 +27,30 @@ class FixDemogalleryCommand extends AppCommand
         $conn = $this->getContainer()->get('database_connection');
         $posts = $conn->fetchAll('SELECT * FROM `iwpf_posts`');
         foreach($posts as $post){
-            $crawler = new Crawler($post['post_content']);
-            $crawler->filter('.demo-gallery')->each(function(&$base, $i){
+            try{
+                $crawler = new Crawler($post['post_content']);
+                $crawler->filter('.demo-gallery')->each(function($base, $i){
 
-                $nodes_li = [];
-                $base->filter('li')->each(function($li, $j) use (&$nodes_li){
-                    $nodes_li[] = $li;
+                    $nodes_li = [];
+                    $base->filter('li')->each(function($li, $j) use (&$nodes_li){
+                        $nodes_li[] = $li;
+                    });
+
+                    $pretty_photo = [];
+                    foreach($nodes_li as $node){
+                        $img = $node->filter('img')->first();
+                        $pretty_photo [] = '<li><a rel="prettyPhoto" href="'.$node->attr('data-src').'" title="'.strip_tags($node->attr('data-sub-html')).'"><img src="'.$img->attr('src').'" alt="'.$img->attr('title').'" title="'.preg_replace('#[a-zA-Z0-9\s]+#Si', ' ', $img->attr('alt')).'"/></a></li>';
+                    }
+
+                    $base->getNode(0)->addHtmlContent('<ul class="sm-gallery">'.implode("", $pretty_photo).'</ul>');
                 });
 
-                $pretty_photo = [];
-                foreach($nodes_li as $node){
-                    $img = $node->filter('img')->first();
-                    $pretty_photo [] = '<li><a rel="prettyPhoto" href="'.$node->attr('data-src').'" title="'.strip_tags($node->attr('data-sub-html')).'"><img src="'.$img->attr('src').'" alt="'.$img->attr('title').'" title="'.preg_replace('#[a-zA-Z0-9\s]+#Si', ' ', $img->attr('alt')).'"/></a></li>';
+                $html = $crawler->html();
+                if(strpos($html, 'sm-gallery')) {
+                    echo $html;
                 }
-
-                $base->getNode(0)->addHtmlContent('<ul class="sm-gallery">'.implode("\n", $pretty_photo).'</ul>');
-            });
-
-            $html = $crawler->html();
-            if(strpos($html, 'sm-gallery')) {
-                echo $html;
+            } catch (Exception $e){
+                echo $e->getMessage();
             }
         }
     }
